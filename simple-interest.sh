fdf2a90306d460151db75fb5710722024adbe57e
Janhavi Gangurde <95962081+Janhavi-Gangurde@users.noreply.github.com>
2024-08-31 23:25:59 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+# Simple Interest Calculator
+echo "Enter principal:"
+read principal
+echo "Enter rate of interest:"
+read rate
+echo "Enter time period:"
+read time
+simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+echo "Simple Interest is: $simple_interest"
 
